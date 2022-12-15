83f84b22c3f9741716bab032e5a082ea8c361342
Chell0 <machelgabriel@gmail.com>
2022-12-15 15:52:45 +0300

    ch: Add a public function

--- ChatAI/APICaller.swift
+++ ChatAI/APICaller.swift
@@ -15,9 +15,22 @@ final class APICaller {
         static let key = "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"
     }
     
+    private var client: OpenAISwift?
+    
     private init() {}
     
     public func setup() {
         let client = OpenAISwift(authToken: Constants.key)
+    }
+    
+    public func getResponse(input: String, completion: @escaping (Result<String, Error>) -> Void) {
+        client?.sendCompletion(with: input, completionHandler: {
+            result in switch result {
+                case .success(let model):
+                    let output = model.choices.first?.text ?? ""
+                    completion(.success(output))
+                case .failure(let error): completion(.failure(error))
+            }
+        })
     }
 }
