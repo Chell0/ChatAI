5c224486bd56c32512b9744300d74b1a96406ad5
Chell0 <machelgabriel@gmail.com>
2022-12-15 17:23:17 +0300

    ch: Add tables

--- ChatAI/ViewController.swift
+++ ChatAI/ViewController.swift
@@ -7,8 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     private let field: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Type here..."
@@ -16,15 +15,32 @@ class ViewController: UIViewController {
         textField.backgroundColor = .red
         return textField
     }()
+    
+    private var models = [String]()
+    
+    private let table: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(field)
+        view.addSubview(table)
+        table.delegate = self
+        table.dataSource = self
         NSLayoutConstraint.activate([
             field.heightAnchor.constraint(equalToConstant: 50),
-            field.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-            field.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
-            field.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor)
+            field.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10),
+            field.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -10),
+            field.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor),
+            
+            table.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            table.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            table.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            table.bottomAnchor.constraint(equalTo: field.topAnchor)
         ])
     }
 
